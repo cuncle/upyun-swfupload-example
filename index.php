6eb41c1eba608cb8598399fed18e85291a89be15
cuncle <uncle5@outlook.com>
2016-03-30 14:57:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php 
-$bucket = 'bucket';//upyun�ռ���
-$form_api_secret = 'form_api_secret';//������Կ����̨����>�ռ䡪��>ͨ�á���>��������
+$bucket = 'bucket';//upyun空间名
+$form_api_secret = 'form_api_secret';//表单密钥：后台——>空间——>通用——>基本设置
 $options = array();
 $options['bucket'] = $bucket;
 $options['expiration'] = time()+600;
-$options['save-key'] = '/{year}/{mon}/{day}/upload_{filename}{.suffix}';//save-key ��ϸ˵�����Կ��ٷ��ĵ�
-$policy = base64_encode(json_encode($options));//policy ����
-$signature = md5($policy.'&'.$form_api_secret);// sigenature����
+$options['save-key'] = '/{year}/{mon}/{day}/upload_{filename}{.suffix}';//save-key 详细说明可以看官方文档
+$policy = base64_encode(json_encode($options));//policy 生成
+$signature = md5($policy.'&'.$form_api_secret);// sigenature生成
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" >
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <head>
 <title>SWFUpload</title>
 <link href="css/default.css" rel="stylesheet" type="text/css" />
@@ -23,17 +23,17 @@ var swfu;
 
 		window.onload = function() {
 			var settings = {
-				flash_url : "swfupload/swfupload.swf",//swfupload.swf�ļ��ľ��Ի���Ե�ַ����Ե�ַ��ָ����ڵ�ǰ��ҳ���ַ
-				upload_url: 'http://v0.api.upyun.com/' + '<?php echo $bucket; ?>',	// API�ϴ���ַ
-				file_post_name : "file", //�൱������ͨ���ļ����ϴ��ļ�ʱ��name���ԣ��������˽���ҳ��ͨ������������ȡ�ϴ����ļ�
+				flash_url : "swfupload/swfupload.swf",//swfupload.swf文件的绝对或相对地址，相对地址是指相对于当前的页面地址
+				upload_url: 'http://v0.api.upyun.com/' + '<?php echo $bucket; ?>',	// API上传地址
+				file_post_name : "file", //相当于用普通的文件域上传文件时的name属性，服务器端接收页面通过该名称来获取上传的文件
 				post_params: {
-				"policy" : "<?php echo $policy;?>",//policy����
-				"signature" : "<?php echo $signature; ?>"},//sigenature��������
-				file_size_limit : "100 MB",//ָ��Ҫ�ϴ����ļ��������������Դ���λ���Ϸ��ĵ�λ��:B��KB��MB��GB�����ʡ���˵�λ����Ĭ��ΪKB��������Ϊ0ʱ����ʾ�������ļ��Ĵ�С��
-				file_types : "*.*", //ָ���������ϴ����ļ����ͣ����ж������ʱʹ�÷ֺŸ��������磺*.jpg;*.png ,������������ʱ��ʹ�� *.*
-				file_types_description : "All Files",//ָ�����ļ�ѡȡ��������ʾ���ļ�������������һ����ʾ��˵�������ð� 
-				file_upload_limit : 100,/*ָ��������ϴ����ٸ��ļ������ϴ��ɹ����ļ������ﵽ��������ֵ�󣬾Ͳ������ϴ��ļ��ˣ�Ҳ�������ϴ������������ļ��ˡ��Ѹ�������Ϊ0ʱ��ʾ�������ļ����ϴ�������*/
-				file_queue_limit : 0, /* ָ���ļ��ϴ������������ͬʱ��Ŷ��ٸ��ļ����������������Ŀ��ֻ�е����������ļ��ϴ��ɹ����ϴ�������ȡ���ϴ��󣬵�ͬ�����������ļ��ſ��Ա����ӽ�������file_upload_limit����ֵ����ʣ������ϴ����ļ�����С��file_queue_limitʱ����ȡ�Ǹ���С��ֵ */
+				"policy" : "<?php echo $policy;?>",//policy传递
+				"signature" : "<?php echo $signature; ?>"},//sigenature参数传递
+				file_size_limit : "100 MB",//指定要上传的文件的最大体积，可以带单位，合法的单位有:B、KB、MB、GB，如果省略了单位，则默认为KB。该属性为0时，表示不限制文件的大小。
+				file_types : "*.*", //指定了允许上传的文件类型，当有多个类型时使用分号隔开，比如：*.jpg;*.png ,允许所有类型时请使用 *.*
+				file_types_description : "All Files",//指定在文件选取窗口中显示的文件类型描述，起一个提示和说明的作用吧 
+				file_upload_limit : 100,/*指定最多能上传多少个文件，当上传成功的文件数量达到了这个最大值后，就不能再上传文件了，也不能往上传队列里添加文件了。把该属性设为0时表示不限制文件的上传数量。*/
+				file_queue_limit : 0, /* 指定文件上传队列里最多能同时存放多少个文件。当超过了这个数目后只有当队列里有文件上传成功、上传出错或被取消上传后，等同数量的其他文件才可以被添加进来。当file_upload_limit的数值或者剩余的能上传的文件数量小于file_queue_limit时，则取那个更小的值 */
 				custom_settings : {
 					progressTarget : "fsUploadProgress",
 					cancelButtonId : "btnCancel"
@@ -45,7 +45,7 @@ var swfu;
 				button_width: "65",
 				button_height: "29",
 				button_placeholder_id: "spanButtonPlaceHolder",
-				button_text: '<span class="theFont">���</span>',
+				button_text: '<span class="theFont">Browse</span>',
 				button_text_style: ".theFont { font-size: 16; }",
 				button_text_left_padding: 12,
 				button_text_top_padding: 3,
